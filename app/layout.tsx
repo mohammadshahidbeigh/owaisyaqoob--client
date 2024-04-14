720983f4b9ce651d0f69813497045458058f4e7d
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-04-14 20:44:21 +0530

    Update global CSS styles and layout in globals.css and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-x-hidden`}>
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
         <Navbar />
-        <main style={{ touchAction: "manipulation" }}>{children}</main>
+        {children}
         <Footer />
       </body>
     </html>
