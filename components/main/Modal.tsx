e93871e19f817231650ce8b69325cf67840dbb85
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-05-15 21:29:57 +0530

    chore: Update Modal close button size and position for improved accessibility

--- components/main/Modal.tsx
+++ components/main/Modal.tsx
@@ -10,11 +10,11 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4 sm:p-6">
-      <div className="bg-white text-black rounded-lg p-8 w-full max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-3xl relative">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-6 sm:p-8 ">
+      <div className="bg-white text-black rounded-lg p-2 w-full max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-3xl relative">
         <button
           onClick={onClose}
-          className="absolute top-4 right-8 text-blue-500  hover:text-blue-700 text-4xl font-bold"
+          className="absolute top-1 right-4 button-2 text-blue-500  hover:text-blue-700 text-4xl font-bold"
         >
           &times;
         </button>
